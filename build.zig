6a55bd4fef86120af5c950b8b53fe0cff112111c
ejmurc <eliasmurcray@gmail.com>
2026-07-04 13:59:52 -0700

    Zig build system

---
+++ build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .freestanding,
+    });
+
+    const wasm = b.addExecutable(.{
+        .name = "tinypx",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = .ReleaseSmall,
+        }),
+    });
+
+    wasm.entry = .disabled;
+    wasm.rdynamic = true;
+
+    b.installArtifact(wasm);
+}
